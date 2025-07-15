8437954fda631075cfb8cde21f04da2df0900d4d
ch-km <c.cpost.plala.jp@gmail.com>
2025-07-15 05:03:08 +0000

    初期ファイルのCleanUp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import './App.css'; //スタイリング用のCSSファイル読み込み
 
+
+// Appという名前の「関数コンポーネント」を定義：Reactアプリのメイン
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  // return の中に、このコンポーネントが画面に何を表示するかを記述。
+  // ここに書かれたHTMLのようなものがブラウザに表示される。
+  return(
+    // <div className="App"> は、見た目を整えるための箱
+    <div className="App">
+      <h1>Hello, React!</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;

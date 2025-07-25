999c4656684943d9567a67abfa4497097a78b239
ch-km <c.cpost.plala.jp@gmail.com>
2025-07-25 04:03:53 +0000

    vite.configの修正

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // '/api' から始まるパスへのリクエストは、
+      // 下の target に指定したサーバーへ転送する
+      '/api': {
+        target: 'http://localhost:5001', // Flaskサーバーのアドレス
+        changeOrigin: true,
+      }
+    }
+  }
 })
